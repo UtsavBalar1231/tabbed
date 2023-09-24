42ccf1a843ccc17b49b3c30650a93b4871ace8ca
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-09-24 19:53:46 +0530

    tabbed: Reconfigure keybindings for personal use
    Change-Id: I0b46e3f29d911b68c657e2bd40f348780ec4e4bb
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- config.h
+++ config.h
@@ -38,13 +38,13 @@ static Bool npisrelative  = False;
 #define MODKEY ControlMask
 static const Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
+	{ MODKEY|ShiftMask,     XK_t, focusonce,   { 0 } },
+	{ MODKEY|ShiftMask,     XK_t, spawn,       { 0 } },
 
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_Right,      rotate,      { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_Left,      rotate,      { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_Up,      movetab,     { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_Down,      movetab,     { .i = +1 } },
 	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
@@ -60,6 +60,7 @@ static const Key keys[] = {
 	{ MODKEY,               XK_0,      move,        { .i = 9 } },
 
 	{ MODKEY,               XK_q,      killclient,  { 0 } },
+	{ MODKEY|ShiftMask,     XK_w,      killclient,  { 0 } },
 
 	{ MODKEY,               XK_u,      focusurgent, { 0 } },
 	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
